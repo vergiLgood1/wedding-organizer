606c88141626ccdd48bfc6d915b2aa9184ad007f
Diyo Anggara <you@example.com git config --global user.email>
2023-10-19 21:38:35 +0700

    firs

---
+++ index.php
@@ -0,0 +1,155 @@
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Wizz</title>
+        <link href=	"https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <link href="style.css" rel="stylesheet">
+        
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap" rel="stylesheet">
+        
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.20.0/font/bootstrap-icons.css" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.20.0/font/bootstrap-icons.css" rel="stylesheet">
+
+
+
+    </head>
+    <body>
+    <!-- Awal Navbar -->
+       <section> <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container">
+            <a class="navbar-brand" href="#"><b><span class="wedding-text">wedding</span></b><b><span class="organizer-text">Organizer</span></b></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav mx-auto">
+                <li class="nav-item">
+                <a class="nav-link active hover-link" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link hover-link" href="#">Vendor</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link hover-link" href="#">Pakaian</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link hover-link" href="#">Undangan</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link hover-link" href="#">Portofolio</a>
+                </li>
+                <li class="nav-item">
+                <a href=""><i class="bi bi-cart-fill"></i></a>
+                </li>
+            </ul>
+            </div>
+           <!-- Awal button navbar -->
+            <div class="buy-button">
+            <button class="btn btn-primary ml-3" type="button">Masuk</button>
+            </div>
+        </div>
+        <!-- Akhir button navbar -->
+        </section>
+      <!-- Akhir Navbar -->
+      
+      <!-- Awal Carousel -->
+        <section>
+            <div class="container">
+                <div class="slider-container">
+                    <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
+                        <div class="carousel-inner">
+                     <div class="carousel-item active">
+                <img src="asset/img/img1.png" class="d-block w-100" alt="Slide 1">
+            </div>
+                    <div class="carousel-item">
+            <img src="asset/img/img2.png" class="d-block w-100" alt="Slide 2">
+        </div>
+        <div class="carousel-item">
+            <img src="asset/img/img3.png" class="d-block w-100" alt="Slide 3">
+        </div>
+        <div class="carousel-item">
+            <img src="asset/img/img4.png" class="d-block w-100" alt="Slide 4">
+        </div>
+    </div>
+    <a class="carousel-control-prev" href="#myCarousel" role="button" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#myCarousel" role="button" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </a>
+        </div>
+    </div>
+</div>
+       
+</section> 
+<!-- Akhir Carousel -->
+
+<!-- Awal Product Category -->
+<section id="Kategori">
+    <div class="container text-center">
+        <div class="container-fluid">
+            <h3>Kategori produk</h3>
+            <div class="row mt-3">
+                <div class="col-4"></div>
+            </div>
+        </div>
+    </div>
+
+</section>
+
+<section class="product-categories">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-2">
+                <div class="category">
+                    <img src="asset/img/PC1.png" alt="Category 1">
+                    <h3>Category 1</h3>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="category">
+                    <img src="asset/img/PC2.png" alt="Category 2">
+                    <h3>Category 2</h3>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="category">
+                    <img src="asset/img/PC3.png" alt="Category 3">
+                    <h3>Category 3</h3>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="category">
+                    <img src="asset/img/PC4.png" alt="Category 4">
+                    <h3>Category 4</h3>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="category">
+                    <img src="asset/img/PC5.png" alt="Category 5">
+                    <h3>Category 5</h3>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="category">
+                    <img src="asset/img/PC6.png" alt="Category 6">
+                    <h3>Category 6</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+   </div>
+        </div>
+</section>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    </body>
+    </html>
