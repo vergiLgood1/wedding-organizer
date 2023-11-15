6686ef93e72c502deced80b44c64a740228b7c89
Alfnnr <alfiannr886@gmail.com>
2023-11-15 08:51:36 +0700

    menambahkan setting untuk about

---
+++ Admin/aboutus.php
@@ -0,0 +1,143 @@
+<?php
+include('security.php');
+include('includes/header.php');
+include('includes/navbar.php');
+?>
+
+
+
+<!-- Modal -->
+<div class="modal fade" id="addadminprofile" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Tambahkan Data About</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+
+      <form action="code.php" method="POST">
+      <div class="modal-body">
+        
+        <div class="form-group">
+            <label> Title </label>
+            <input type="text" name="judul" class="form-control" placeholder="Enter Title">
+        </div>
+        <div class="form-group">
+            <label> Sub Title </label>
+            <input type="text" name="sub_judul" class="form-control" placeholder="Enter Sub Title">
+        </div>
+        <div class="form-group">
+            <label> Description </label>
+            <input type="text" name="deskripsi" class="form-control" placeholder="Enter Description">
+        </div>
+        <div class="form-group">
+            <label> Links </label>
+            <input type="text" name="links" class="form-control" placeholder="Enter Links">
+        </div>
+        
+
+        <input type="hidden" name="usertype" value="admin">
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="submit" name="about_save" class="btn btn-primary">Save changes</button>
+      </div>
+      </form>
+
+    </div>
+  </div>
+</div>
+
+<div class="container-fluid">
+<!--DataTables Example -->
+<div class="card shadow mb-4">
+    <div class="card-header py-3">
+        <h6 class="m-0 font-weight-bold text-primary">About
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#addadminprofile">
+                add About
+            </button>
+        </h6>
+    </div>    
+
+<div class="card-body">
+
+<?php
+    if(isset($_SESSION['success']) && $_SESSION['success'] !='')
+    {
+        echo '<h2 class="bg-primary text-white">'.$_SESSION['success'].'</h2>';
+        unset($_SESSION['success']);
+    }
+
+    if(isset($_SESSION['status']) && $_SESSION['status'] !='')
+    {
+        echo '<h2 class="bg-danger text-white">'.$_SESSION['status'].'</h2>';
+        unset($_SESSION['status']);
+    }
+
+    ?>
+
+    <div class="table-responsive">
+        
+    
+        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Title</th>
+                    <th>Sub Title</th>
+                    <th>Description</th>
+                    <th>Links</th>
+                    <th>EDIT</th>
+                    <th>Delete</th>    
+                </tr>    
+            </thead>
+            <tbody>
+            
+            <?php
+            $query = "SELECT * FROM abouts";
+            $query_run = mysqli_query($connection, $query);
+            if(mysqli_num_rows($query_run) > 0)
+            {
+                while($row = mysqli_fetch_assoc($query_run))
+                {
+                    ?>
+                    <tr>
+                    <td><?php echo $row['id']; ?></td>
+                    <td><?php echo $row['title']; ?></td>
+                    <td><?php echo $row['subtitle']; ?></td>
+                    <td><?php echo $row['description']; ?></td>
+                    <td><?php echo $row['links']; ?></td>
+                    <td>
+                        <form action="aboutusedit.php" method="POST">
+                        <input type="hidden" name="edit_id" value="<?php echo $row['id']; ?>">    
+                        <button type="submit" name="edit_btn" class="btn btn-success">EDIT</button>
+                        </form>
+                    </td>
+                    <td>
+                        <form action="code.php" method="post">
+                        <input type="hidden" name="delete_id" value="<?php echo $row['id']; ?>">    
+                        <button type="submit" name="deletebtn" class="btn btn-danger">DELETE</button>
+                        </form>
+                    </td>
+                </tr>
+                    <?php
+                }
+            }
+            else{
+                echo "Tidak ada Record data";
+            }
+            ?>  
+            </tbody>    
+        </table>
+
+        </div>
+    </div>
+</div>
+
+
+
+<?php
+include('includes/scripts.php');
+include('includes/footer.php');
+?>
