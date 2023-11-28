ea4a182d0acecac13f11caa803be85f532c653de
putr14yu <148509979+putr14yu@users.noreply.github.com>
2023-11-28 14:07:30 +0700

    tambah revisi login

---
+++ Login Revisi/Login2/Register/phpdaftar.php
@@ -0,0 +1,12 @@
+<?php
+require ('../Koneksi/koneksi.php');
+if(isset($_POST['register'])) {
+    $userMail = $_POST['email'];
+    $userPass = $_POST['password'];
+    $userName = $_POST['username'];
+
+    $query = "INSERT INTO admin VALUES ('', '$userName', '$userMail', '$userPass', 2)";
+    $result = mysqli_query($koneksi, $query);
+    header('Location: ../Login/login.php');
+} 
+?>
